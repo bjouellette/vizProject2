d68f6e53f9d8dc5cba60c15f850bff456ae80e4a
bjouellette <67930251+bjouellette@users.noreply.github.com>
2020-11-11 21:09:33 -0600

    slick added

---
+++ JS/custom.js
@@ -0,0 +1,11 @@
+(function() {
+      (document).ready(function() {
+          ('.slider-hero').slick({
+              dots: true,
+              infinite: true,
+              cssEase: 'linear',
+              swipe: false,
+          });
+      });
+    
+  })( jQuery );
