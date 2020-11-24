17a19b644749b0c5cfad408e16b14ef9a7c78d55
bjouellette <67930251+bjouellette@users.noreply.github.com>
2020-11-23 20:12:50 -0600

    update

---
+++ static/bar2.js
@@ -0,0 +1,78 @@
+﻿function getplot(Year) {
+    // Load data from HappinessData file
+    d3.csv(`/Data/2015 - 2019 Happiness Report Data.csv`).then(function (data) {
+        console.log(data);​
+        // //filter data values by year 
+        // var filteredData = data.filter(d => d.Year.toString() === Year)[0];
+
+        // Create a custom filtering function
+        function selectYear(Happy) {
+            return Happy.Year == Year;
+        }​
+        // filter() uses the custom function as its argument
+        var filteredData = data.filter(selectYear);
+
+        console.log(filteredData);​
+        // Sort filterd data by Happiness Score -descending
+        var sortedByHappinessScore = filteredData.sort((a, b) => b.Happiness_Score - a.Happiness_Score);
+
+        // Slice the first 10 objects for plotting - Top Ten
+        slicedData = sortedByHappinessScore.slice(0, 10);​
+        // Sort filterd data by Happiness Score -ascending
+        var sortedByAscending = filteredData.sort((a, b) => a.Happiness_Score - b.Happiness_Score);​
+        // Slice the first 10 objects for plotting - Top Ten for ascending data
+        AscendingslicedData = sortedByAscending.slice(0, 10);
+
+        // Use the map method with the arrow function to return all the filtered countries.
+        var countries = slicedData.map(slice => slice.Country);
+
+        // Use the map method with the arrow function to return all the filtered countries.
+        var countriesAscending = AscendingslicedData.map(slice => slice.Country);​
+        var ratings = slicedData.map(slice => slice.Happiness_Score);​
+        var ratingsAscending = AscendingslicedData.map(slice => slice.Happiness_Score)​
+        // Create your trace.
+        var trace = {
+            x: countries,
+            y: ratings,
+            type: "bar"
+        };​
+        // Create the data array for our plot
+        var data = [trace];​
+        // Define our plot layout
+        var layout = {
+            title: "Top Happy Countries",
+            xaxis: {
+                title: "Countries"
+            },
+            yaxis: {
+                title: "HappinessScore"
+            }
+        };​
+        //  Plot the chart to a div tag with id "bar"
+        Plotly.newPlot("bar", data, layout);​
+        // Create your trace.
+        var trace2 = {
+            x: countriesAscending,
+            y: ratingsAscending,
+            type: "bar"
+        };​
+        // Create the data array for our plot
+        var data2 = [trace2];​
+        // Define our plot layout
+        var layout2 = {
+            title: "Least Happy Countries",
+            xaxis: {
+                title: "Countries"
+            },
+            yaxis: {
+                title: "HappinessScore"
+            }
+        };​
+        //  Plot the chart to a div tag with id "bar-plot"
+        Plotly.newPlot("bar-plot", data2, layout2);​
+    });​
+}​
+function optionChanged(Year) {
+    getplot(Year);
+    console.log(Year)
+}​
--- static/scatter2.js
+++ static/scatter2.js
@@ -1,8 +1,6 @@
 //define svg
-var svgHeight = 800
+var svgHeight = 800;
 var svgWidth = 1000
-
-
 //set margins
 var margin = {
     top: 10,
@@ -10,20 +8,16 @@ var margin = {
     right: 10,
     left: 60,
 };
-
 var chartWidth = svgWidth - margin.left - margin.right;
 var chartHeight = svgHeight - margin.top - margin.bottom;
-
 //svg wrapper
 var svg = d3.select("#scatter")
     .append("svg")
     .attr("height", svgHeight)
     .attr("width", svgWidth);
-
 //append SVG group
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
 //initial params
 var yAxisVar = "Economy";
 var yearSelect = "2019";
@@ -31,7 +25,6 @@ var xAxisVar = "Happiness_Score"
 // var yearSelect = d3.select("#year-input");
 var filtered;
 var varValue;
-
 //updating yaxis scale
 function yScale(filtered, yAxisVar) {
     var yLinearScale = d3.scaleLinear()
@@ -41,7 +34,6 @@ function yScale(filtered, yAxisVar) {
         .range([chartHeight, 0]);
     return yLinearScale;
 };
-
 //updating xaxis scale
 function xScale(filtered, xAxisVar) {
     var xLinearScale = d3.scaleLinear()
@@ -54,48 +46,48 @@ function xScale(filtered, xAxisVar) {
 //updating yaxis
 function renderYAxes(yLinearScale, yAxis) {
     var leftAxis = d3.axisLeft(yLinearScale);
-
+​
     yAxis.transition()
         .duration(1000)
         .call(leftAxis);
-
+​
     return yAxis;
 };
 //updating xaxis
 function renderXAxes(xLinearScale, xAxis) {
     var bottomAxis = d3.axisBottom(xLinearScale);
-
+​
     xAxis.transition()
         .duration(1000)
         .call(bottomAxis);
-
+​
     return xAxis;
 };
-
+​
 //updating circles
 function renderCircles(circlesGroup, yLinearScale, yAxisVar) {
-
+​
     circlesGroup.transition()
         .duration(1000)
         .attr("cy", d => yLinearScale(d[yAxisVar]))
         .attr("cx", d => xLinearScale(d[xAxisVar]))
-
-
+​
+​
     return circlesGroup;
 };
 // function renderXCircles(circlesGroup, xLinearScale, xAxisVar) {
-
+​
 //     circlesGroup.transition()
 //         .duration(1000)
 //         .attr("cx", d => xLinearScale(d[xAxisVar]));
-
+​
 //     return circlesXGroup;
 // };
 //updating ToolTip 
 function updateToolTip(yAxisVar, circlesGroup) {
-
+​
     var label;
-
+​
     if (yAxisVar === "Economy") {
         label = "Economy:";
     }
@@ -114,16 +106,16 @@ function updateToolTip(yAxisVar, circlesGroup) {
     if (yAxisVar === "Generosity") {
         label = "Generosity:";
     };
-
+​
     var toolTip = d3.tip()
         .attr("class", "tooltip")
         .offset([80, -60])
         .html(function (d) {
             return (`${d.Country}<br> Happiness Score: ${d.Happiness_Score}<br>${label} ${d[yAxisVar]}`);
         });
-
+​
     circlesGroup.call(toolTip);
-
+​
     circlesGroup.on("mouseover", function (data) {
         toolTip.show(data, this);
     })
@@ -131,7 +123,7 @@ function updateToolTip(yAxisVar, circlesGroup) {
         .on("mouseout", function (data, index) {
             toolTip.hide(data);
         });
-
+​
     return circlesGroup;
 }
 // //updating Chart labels function--right now overwriting previous label
@@ -146,14 +138,14 @@ function updateToolTip(yAxisVar, circlesGroup) {
 //     //     .text(`${yAxisVar}`)
 //         d3.selectAll(chartGroup).text= "something";
 // }
-
-
+​
+​
 //Initial axis labels
 chartGroup.append("text")
     .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top + 15})`)
     .attr("class", "axisText")
     .text(`Happiness Score`);
-
+​
 // chartGroup.append("text")
 //     .attr("transform", "rotate(-90)")
 //     .attr("y", 0 - margin.left)
@@ -161,7 +153,7 @@ chartGroup.append("text")
 //     .attr("dy", "1em")
 //     .attr("class", "axisText")
 //     .text(`${yAxisVar}`);
-
+​
 //import data
 d3.csv("/Data/data.csv").then(function (happyData) {
     // var filtered, x, y, yAxisVar, varValue;
@@ -176,11 +168,11 @@ d3.csv("/Data/data.csv").then(function (happyData) {
         data.Freedom = +data.Freedom;
         data.Trust = +data.Trust;
         data.Country = data.Country
-
+​
     });
     //filtered data
     filtered = happyData.filter(data => (data.Year == yearSelect));
-
+​
     //xScale
     // var xAxis = filtered.map(data => data["Happiness_Score"]);
     // var xLinearScale = d3.scaleLinear()
@@ -189,11 +181,11 @@ d3.csv("/Data/data.csv").then(function (happyData) {
     var xLinearScale = xScale(filtered, xAxisVar);
     //yScale
     var yLinearScale = yScale(filtered, yAxisVar);
-
+​
     //year event listener
     yearSelect = d3.select("#year-input");
-
-
+​
+​
     //Select year filter
     yearSelect.on("change", () => {
         d3.event.preventDefault();
@@ -212,43 +204,43 @@ d3.csv("/Data/data.csv").then(function (happyData) {
         // circlesGroup = updateLabels(yAxisVar, chartGroup);
         console.log(yearValue);
         console.log(filtered);
-
-
+​
+​
     });
-
-
-
+​
+​
+​
     //create initial params
-
+​
     // var Economy = filtered.map(data => data["Economy"]);
     // console.log(Economy);
-
+​
     // yAxisVar = "Economy"
-
+​
     //function for updating yaxis scale
-
+​
     // ([0, d3.max(yAxisVar)])
     // .range([chartHeight, 0])
-
+​
     // Create initial axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
-
+​
     // append y axis --flip location of chart width
     var yAxis = chartGroup.append("g")
         .classed("y-axis", true)
         .attr("transform", `translate(${chartWidth},0)`)
         .call(leftAxis);
-
+​
     var xAxis = chartGroup.append("g")
         .classed("x-axis", true)
         .attr("transform", `translate(0, ${chartHeight})`)
         .call(bottomAxis);
-
+​
     // append x axis
     // chartGroup.append("g")
     //     .call(bottomAxis);
-
+​
     // append initial circles
     var circlesGroup = chartGroup.selectAll("circle")
         .data(filtered)
@@ -259,18 +251,18 @@ d3.csv("/Data/data.csv").then(function (happyData) {
         .attr("r", 5)
         .attr("fill", "blue")
         .attr("opacity", ".5");
-
-
+​
+​
     // updateToolTip function above csv import
     //   var circlesGroup = updateToolTip(yAxisVar, circlesGroup);
     // var varSelect = "Economy"
     varSelect = d3.select("#varvar");
-
-
+​
+​
     varSelect.on("change", () => {
         // d3.event.preventDefault();
         // varValue = varSelect.property("value");
-
+​
         yAxisVar = varSelect.property("value");
         // xAxisVar = filtered.map(data => data[xAxisVar])
         // yAxisVar = filtered.map(data => data[varValue]);
@@ -288,7 +280,7 @@ d3.csv("/Data/data.csv").then(function (happyData) {
         // console.log(varValue);
         console.log(yAxisVar)
         // onUpdate(d3.max(yAxisVar))
-
+​
         //change to on update
         chartGroup = updateLabels(yAxisVar, chartGroup)
         // chartGroup.append("text")
@@ -298,12 +290,10 @@ d3.csv("/Data/data.csv").then(function (happyData) {
         //     .attr("dy", "1em")
         //     .attr("class", "axisText")
         //     .text(`${yAxisVar}`);
-
+​
     });
 });
 // Create axes labels
-
-
-
-
-
+​
+​
+​
